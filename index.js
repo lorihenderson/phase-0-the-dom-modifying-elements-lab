db56083c90a9dd40a7520c3a0c5c0501a01033d1
Lori Henderson <lorim95@gmail.com>
2022-05-23 18:05:03 -0400

    commit tree f4a50c82cdba8440f6ab0c89a128de3645f6b42d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion";
+// const newHeader = document.createElement("#victory");
